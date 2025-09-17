517d91b2b4f922f4bdb541ff0959c598e26ea4bb
yfu <fyvo1d@outlook.com>
2025-09-17 16:57:58 +0800

    median of medians select

--- cpp/order-statistics/main.cpp
+++ cpp/order-statistics/main.cpp
@@ -6,24 +6,31 @@
 
 void check_selection(const std::vector<int>& sorted, size_t k, int value) {
     if (value != sorted[k]) {
-        std::cout << "Error: value does not match sorted[k]" << std::endl;
+        throw std::runtime_error("Selection algorithm failed");
     }
     std::cout << "Check complete" << std::endl;
 }
 
 int main() {
     auto data = SelectionAlgorithm<int>::shuffle(SelectionAlgorithm<int>::make_data(10000));
-    auto k = data.size() / 2;
+    auto k = get_rand<size_t>(0, data.size() - 1);
     auto sorted = data;
     std::sort(sorted.begin(), sorted.end());
+    
+    std::cout << "k: " << k << std::endl;
 
     time_it([data, sorted, k]() {
-        auto [index, value] = SelectionAlgorithm<int>::linear_select(data, k);
+        auto value = SelectionAlgorithm<int>::linear_select(data, k);
         check_selection(sorted, k, value);
     }, "Linear(Sort) Selection Algorithm");
 
     time_it([data, sorted, k]() {
-        auto [index, value] = SelectionAlgorithm<int>::random_select(data, k);
+        auto value = SelectionAlgorithm<int>::random_select(data, k);
         check_selection(sorted, k, value);
     }, "Randomized Selection Algorithm");
+
+    time_it([data, sorted, k]() {
+        auto value = SelectionAlgorithm<int>::median_of_medians_select(data, k);
+        check_selection(sorted, k, value);
+    }, "Median of Medians Selection Algorithm");
 }
--- cpp/order-statistics/select.hpp
+++ cpp/order-statistics/select.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
+#include "helper.hpp"
 #include <concepts>
 #include <vector>
 #include <tuple>
 #include <algorithm>
+#include <iostream>
 
 template<std::totally_ordered T>
 class SelectionAlgorithm {
@@ -11,7 +13,7 @@ public:
     static std::vector<T> shuffle(const std::vector<T>& data) {
         auto ret = data;
         for (size_t i = 0; i < ret.size(); i++) {
-            auto j = get_rand<size_t>(0, ret.size() - 1);
+            auto j = get_rand<size_t>(i, ret.size() - 1);
             std::swap(ret[i], ret[j]);
         }
         return ret;
@@ -23,43 +25,34 @@ public:
         }
         return ret;
     }
-    static std::tuple<size_t, T> linear_select(const std::vector<T>& data, size_t k) {
+    static T linear_select(const std::vector<T>& data, size_t k) {
         auto cpy = data;
         std::sort(cpy.begin(), cpy.end());
-        return {k, cpy[k]};
+        return cpy[k];
     }
 private:
+    static size_t partition(std::vector<T>& data, size_t pivot_index, size_t begin, size_t end) {
+        auto pivot = data[pivot_index];
+        std::swap(data[pivot_index], data[end - 1]);
+
+        size_t store_index = begin;
+        for (size_t i = begin; i < end - 1; i++) {
+            if (data[i] < pivot) {
+                std::swap(data[i], data[store_index]);
+                store_index++;
+            }
+        }
+        std::swap(data[store_index], data[end - 1]);
+        return store_index;
+    }
     static size_t random_partition(std::vector<T>& data, size_t begin, size_t end) {
         auto pivot_index = get_rand<size_t>(begin, end - 1);
-        std::swap(data[begin], data[pivot_index]);
-
-        auto pivot = data[begin];
-        auto left = begin + 1;
-        auto right = end - 1;
-
-        while (true) {
-            while (data[left] < pivot) {
-                left++;
-            }
-            while (data[right] > pivot) {
-                right--;
-            }
-            if (left >= right) {
-                break;
-            }
-            std::swap(data[left], data[right]);
-            left++;
-            right--;
-        }
-
-        std::swap(data[begin], data[right]);
-
-        return right;
+        return partition(data, pivot_index, begin, end);
     }
-    static std::tuple<size_t, T> rselect(std::vector<T>& data, size_t k, size_t begin, size_t end) {
+    static T rselect(std::vector<T>& data, size_t k, size_t begin, size_t end) {
         auto partition = random_partition(data, begin, end);
         if (partition == k) {
-            return {partition, data[partition]};
+            return data[partition];
         }
         else if (partition > k) {
             return rselect(data, k, begin, partition);
@@ -69,8 +62,46 @@ private:
     }
 public:
     // return k-th smallest element and its index in the original array
-    static std::tuple<size_t, T> random_select(const std::vector<T>& data, size_t k) {
+    static T random_select(const std::vector<T>& data, size_t k) {
         auto cpy = data;
         return rselect(cpy, k, 0, cpy.size());
+    }
+private:
+    static const size_t MOM_GROUP_SIZE = 5;
+    static size_t select(std::vector<T>& data, size_t begin, size_t end) {
+        if (end - begin <= MOM_GROUP_SIZE) {
+            std::sort(data.begin() + begin, data.begin() + end);
+            return begin + (end - 1 - begin) / 2;
+        }
+
+        int medians = 0;
+        for (size_t i = begin; i < end; i += MOM_GROUP_SIZE) {
+            size_t group_end = std::min(i + MOM_GROUP_SIZE, end);
+            std::sort(data.begin() + i, data.begin() + group_end);
+            size_t median_index = i + (group_end - 1 - i) / 2;
+            std::swap(data[begin + (i - begin) / MOM_GROUP_SIZE], data[median_index]);
+            medians++;
+        }
+
+        return select(data, begin, begin + medians);
+    }
+    static T mom(std::vector<T>& data, size_t k, size_t begin, size_t end) {
+        size_t pivot_index = select(data, begin, end);
+        
+        pivot_index = partition(data, pivot_index, begin, end);
+
+        if (pivot_index == k) {
+            return data[pivot_index];
+        }
+        else if (pivot_index > k) {
+            return mom(data, k, begin, pivot_index);
+        } else {
+            return mom(data, k, pivot_index + 1, end);
+        }
+    }
+public:
+    static T median_of_medians_select(const std::vector<T>& data, size_t k) {
+        auto cpy = data;
+        return mom(cpy, k, 0, cpy.size());
     }
 };
